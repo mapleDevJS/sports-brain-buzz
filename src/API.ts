ef4b658db7086e1d0ed65ca0b5aabf7ab5e56d47
Alexey Ivanov <alexey.ivanov.js@gmail.com>
2024-06-21 22:01:05 -0400

    Update API endpoint to include category
    The commit adds a category parameter to the endpoint URL in 'fetchQuizQuestions' function in API.ts file. This lets the function pull questions from a specific category, increasing the flexibility of the API call.

--- src/API.ts
+++ src/API.ts
@@ -18,7 +18,7 @@ export enum Difficulty {
 export type QuestionsState = Question & { answers: string[] };
 
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty.EASY): Promise<QuestionsState[]> => {
-    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=21&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
     return data.results.map((question: Question) => ({
         ...question,
